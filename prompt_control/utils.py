7c58a1a995bf7ea7a233bbe4509d08316c03954b
asagi4 <130366179+asagi4@users.noreply.github.com>
2023-08-26 13:27:17 +0300

    Adapt to ComfyUI refactoring and drop support for older versions of AITemplate

--- prompt_control/utils.py
+++ prompt_control/utils.py
@@ -5,6 +5,7 @@ import comfy.sample
 import comfy.samplers
 import comfy.utils
 import comfy.sd
+import comfy.lora
 import folder_paths
 
 import logging
@@ -24,43 +25,32 @@ def steps(start, end=None, step=0.1):
         start = round(start, 2)
 
 
-def untuple(model):
-    if isinstance(model, tuple):
-        return model[0]
-    else:
-        return model
-
-
 def get_aitemplate_module():
     return sys.modules["AIT.AITemplate.AITemplate"]
 
 
 def unpatch_model(model):
-    untuple(model).unpatch_model()
+    model.unpatch_model()
 
 
 def clone_model(model):
-    if isinstance(model, tuple):
-        return (model[0].clone(), model[1])
-    else:
         return model.clone()
 
 
 def add_patches(model, patches, weight):
-    untuple(model).add_patches(patches, weight)
+    model.add_patches(patches, weight)
 
 
 def patch_model(model):
-    if isinstance(model, tuple) or "aitemplate_keep_loaded" in model.model_options:
-        m = untuple(model)
-        m.patch_model()
+    if "aitemplate_keep_loaded" in model.model_options:
+        model.patch_model()
         mod = get_aitemplate_module()
         l = mod.AITemplate.loader
         if hasattr(l, "pc_applied_module"):
             log.info("Applying AITemplate unet")
             l.apply_unet(
                 aitemplate_module=l.pc_applied_module,
-                unet=l.compvis_unet(m.model.state_dict()),
+                unet=l.compvis_unet(model.model.state_dict()),
                 in_channels=m.model.diffusion_model.in_channels,
                 conv_in_key="conv_in_weight",
             )
@@ -70,20 +60,19 @@ def patch_model(model):
 
 
 def get_callback(model):
-    return untuple(model).model_options.get("prompt_control_callback")
+    return model.model_options.get("prompt_control_callback")
 
 
 def set_callback(model, cb):
-    untuple(model).model_options["prompt_control_callback"] = cb
+    model.model_options["prompt_control_callback"] = cb
 
 
 def get_lora_keymap(model=None, clip=None):
     key_map = {}
     if model:
-        model = untuple(model)
-        key_map = comfy.sd.model_lora_keys_unet(model.model)
+        key_map = comfy.lora.model_lora_keys_unet(model.model)
     if clip:
-        key_map = comfy.sd.model_lora_keys_clip(clip.cond_stage_model, key_map)
+        key_map = comfy.lora.model_lora_keys_clip(clip.cond_stage_model, key_map)
     return key_map
 
 
@@ -94,7 +83,7 @@ def load_lora(model, lora, weight, key_map, clone=True):
 
     p = print
     __builtins__["print"] = noop
-    loaded = comfy.sd.load_lora(lora, key_map)
+    loaded = comfy.lora.load_lora(lora, key_map)
     __builtins__["print"] = p
     if clone:
         model = clone_model(model)
