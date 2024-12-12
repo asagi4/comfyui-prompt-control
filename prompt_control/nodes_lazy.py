c21a31cd94f5da26cbdcda43e53f22c4c8bef308
asagi4 <130366179+asagi4@users.noreply.github.com>
2024-12-12 23:08:11 +0200

    Lazy nodes with filtering

--- prompt_control/nodes_lazy.py
+++ prompt_control/nodes_lazy.py
@@ -67,6 +67,87 @@ def create_hook_nodes_for_lora(graph, path, info, existing_node, start_pct, end_
     return hook_node, next_keyframe
 
 
+def build_lora_schedule(graph, schedule, model, clip, apply_hooks):
+    # This gets rid of non-existent LoRAs
+    consolidated = consolidate_schedule(schedule)
+    non_scheduled = find_nonscheduled_loras(consolidated)
+    model, clip = create_lora_loader_nodes(graph, model, clip, non_scheduled)
+
+    hook_nodes = {}
+    start_pct = 0.0
+
+    def key(lora, info):
+        return f"{lora}-{info['weight']}-{info['weight_clip']}"
+
+    for end_pct, loras in consolidated:
+        for lora, info in loras.items():
+            if non_scheduled.get(lora) == info:
+                continue
+            k = key(lora, info)
+            existing_node = hook_nodes.get(k)
+            hook_nodes[k] = create_hook_nodes_for_lora(graph, lora, info, existing_node, start_pct, end_pct)
+        start_pct = end_pct
+
+    hooks = []
+    # Attach the keyframe chain to the hook node
+    for hook, kfs in hook_nodes.values():
+        n = graph.node("SetHookKeyframes")
+        n.set_input("hooks", hook.out(0))
+        n.set_input("hook_kf", kfs.out(0))
+        hooks.append(n)
+
+    res = None
+    # Finally, combine all hooks and optionally apply
+    if len(hooks) > 0:
+        res = hooks[0]
+        for h in hooks[:1]:
+            n = graph.node("CombineHooks2")
+            n.set_input("hooks_A", res.out(0))
+            n.set_input("hooks_B", h.out(0))
+            res = n
+        res = res.out(0)
+    if apply_hooks:
+        n = graph.node("SetClipHooks")
+        n.set_input("clip", clip)
+        n.set_input("hooks", res)
+        n.set_input("apply_to_conds", True)
+        n.set_input("schedule_clip", True)
+        clip = n.out(0)
+
+    r = graph.finalize()
+
+    return {"result": (model, clip, res), "expand": r}
+
+
+class PCLazyLoraLoaderAdvanced:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {
+                "text": ("STRING", {"multiline": True}),
+                "model": ("MODEL", {"rawLink": True}),
+                "clip": ("CLIP", {"rawLink": True}),
+                "apply_hooks": ("BOOLEAN", {"default": True}),
+            },
+            "optional": {
+                "tags": ("STRING", {"default": ""}),
+                "start": ("FLOAT", {"min": 0.0, "max": 1.0, "default": 0.0, "step": 0.01}),
+                "end": ("FLOAT", {"min": 0.0, "max": 1.0, "default": 1.0, "step": 0.01}),
+            },
+            "hidden": {"unique_id": "UNIQUE_ID"},
+        }
+
+    RETURN_TYPES = ("MODEL", "CLIP", "HOOKS")
+    OUTPUT_TOOLTIPS = ("Returns a model and clip with LoRAs scheduled",)
+    CATEGORY = "promptcontrol"
+    FUNCTION = "apply"
+
+    def apply(self, model, clip, text, apply_hooks, unique_id, tags="", start=0.0, end=1.0):
+        schedule = parse_prompt_schedules(text).with_filters(tags=tags, start=start, end=end)
+        graph = GraphBuilder(f"PCLazyLoraLoaderAdvanced-{unique_id}")
+        return build_lora_schedule(graph, schedule, model, clip, apply_hooks)
+
+
 class PCLazyLoraLoader:
     @classmethod
     def INPUT_TYPES(s):
@@ -86,57 +167,43 @@ class PCLazyLoraLoader:
     FUNCTION = "apply"
 
     def apply(self, model, clip, text, apply_hooks, unique_id):
-        schedule = parse_prompt_schedules(text)
-        # This gets rid of non-existent LoRAs
-        consolidated = consolidate_schedule(schedule)
-        non_scheduled = find_nonscheduled_loras(consolidated)
         graph = GraphBuilder(f"PCLazyLoraLoader-{unique_id}")
-        model, clip = create_lora_loader_nodes(graph, model, clip, non_scheduled)
+        schedule = parse_prompt_schedules(text)
+        return build_lora_schedule(graph, schedule, model, clip, apply_hooks)
 
-        hook_nodes = {}
-        start_pct = 0.0
 
-        def key(lora, info):
-            return f"{lora}-{info['weight']}-{info['weight_clip']}"
+def build_scheduled_prompts(graph, schedules, clip):
+    nodes = []
+    start_pct = 0.0
+    prompt_cache = {}
+    for end_pct, c in schedules:
+        p = c["prompt"]
+        p, classnames = get_function(p, "NODE", ["PCTextEncode", "text"])
+        classname = "PCTextEncode"
+        paramname = "text"
+        if classnames:
+            classname = classnames[0][0]
+            paramname = classnames[0][1]
+        node = prompt_cache.get((p, classname, paramname))
+        if not node:
+            node = graph.node(classname)
+            node.set_input("clip", clip)
+            node.set_input(paramname, p)
+            prompt_cache[(p, classname, paramname)] = node
+        timestep = graph.node("ConditioningSetTimestepRange")
+        timestep.set_input("conditioning", node.out(0))
+        timestep.set_input("start", start_pct)
+        timestep.set_input("end", end_pct)
+        nodes.append(timestep)
+        start_pct = end_pct
+    node = nodes[0]
+    for othernode in nodes[1:]:
+        combiner = graph.node("ConditioningCombine")
+        combiner.set_input("conditioning_1", node.out(0))
+        combiner.set_input("conditioning_2", othernode.out(0))
+        node = combiner
 
-        for end_pct, loras in consolidated:
-            for lora, info in loras.items():
-                if non_scheduled.get(lora) == info:
-                    continue
-                k = key(lora, info)
-                existing_node = hook_nodes.get(k)
-                hook_nodes[k] = create_hook_nodes_for_lora(graph, lora, info, existing_node, start_pct, end_pct)
-            start_pct = end_pct
-
-        hooks = []
-        # Attach the keyframe chain to the hook node
-        for hook, kfs in hook_nodes.values():
-            n = graph.node("SetHookKeyframes")
-            n.set_input("hooks", hook.out(0))
-            n.set_input("hook_kf", kfs.out(0))
-            hooks.append(n)
-
-        res = None
-        # Finally, combine all hooks and optionally apply
-        if len(hooks) > 0:
-            res = hooks[0]
-            for h in hooks[:1]:
-                n = graph.node("CombineHooks2")
-                n.set_input("hooks_A", res.out(0))
-                n.set_input("hooks_B", h.out(0))
-                res = n
-            res = res.out(0)
-        if apply_hooks:
-            n = graph.node("SetClipHooks")
-            n.set_input("clip", clip)
-            n.set_input("hooks", res)
-            n.set_input("apply_to_conds", True)
-            n.set_input("schedule_clip", True)
-            clip = n.out(0)
-
-        r = graph.finalize()
-
-        return {"result": (model, clip, res), "expand": r}
+    return {"result": (node.out(0),), "expand": graph.finalize()}
 
 
 class PCLazyTextEncode:
@@ -156,47 +223,42 @@ class PCLazyTextEncode:
     def apply(self, clip, text, unique_id):
         schedules = parse_prompt_schedules(text)
         graph = GraphBuilder(f"PCEncodeLazy-{unique_id}")
+        return build_scheduled_prompts(graph, schedules, clip)
 
-        nodes = []
-        start_pct = 0.0
-        prompt_cache = {}
-        for end_pct, c in schedules:
-            p = c["prompt"]
-            p, classnames = get_function(p, "NODE", ["PCTextEncode", "text"])
-            classname = "PCTextEncode"
-            paramname = "text"
-            key = (p, classname, paramname)
-            if classnames:
-                classname = classnames[0][0]
-                paramname = classnames[0][1]
-            node = prompt_cache.get((p, classname, paramname))
-            if not node:
-                node = graph.node(classname)
-                node.set_input("clip", clip)
-                node.set_input(paramname, p)
-                prompt_cache[(p, classname, paramname)] = node
-            timestep = graph.node("ConditioningSetTimestepRange")
-            timestep.set_input("conditioning", node.out(0))
-            timestep.set_input("start", start_pct)
-            timestep.set_input("end", end_pct)
-            nodes.append(timestep)
-            start_pct = end_pct
-        node = nodes[0]
-        for othernode in nodes[1:]:
-            combiner = graph.node("ConditioningCombine")
-            combiner.set_input("conditioning_1", node.out(0))
-            combiner.set_input("conditioning_2", othernode.out(0))
-            node = combiner
 
-        return {"result": (node.out(0),), "expand": graph.finalize()}
+class PCLazyTextEncodeAdvanced:
+    @classmethod
+    def INPUT_TYPES(s):
+        return {
+            "required": {"clip": ("CLIP", {"rawLink": True}), "text": ("STRING", {"multiline": True})},
+            "optional": {
+                "tags": ("STRING", {"default": ""}),
+                "start": ("FLOAT", {"min": 0.0, "max": 1.0, "default": 0.0, "step": 0.01}),
+                "end": ("FLOAT", {"min": 0.0, "max": 1.0, "default": 1.0, "step": 0.01}),
+            },
+            "hidden": {"unique_id": "UNIQUE_ID"},
+        }
+
+    RETURN_TYPES = ("CONDITIONING",)
+    CATEGORY = "promptcontrol"
+    FUNCTION = "apply"
+
+    def apply(self, clip, text, unique_id, tags="", start=0.1, end=1.0):
+        schedules = parse_prompt_schedules(text).with_filters(start=start, end=end, tags=tags)
+        graph = GraphBuilder(f"PCLazyTextEncodeAdvanced-{unique_id}")
+        return build_scheduled_prompts(graph, schedules, clip)
 
 
 NODE_CLASS_MAPPINGS = {
     "PCLazyTextEncode": PCLazyTextEncode,
+    "PCLazyTextEncodeAdvanced": PCLazyTextEncodeAdvanced,
     "PCLazyLoraLoader": PCLazyLoraLoader,
+    "PCLazyLoraLoaderAdvanced": PCLazyLoraLoaderAdvanced,
 }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "PCLazyTextEncode": "Encode prompt w/ scheduling (Lazy)",
-    "PCLazyLoraLoader": "Load LoRAs from prompt w/ scheduling (Lazy)",
+    "PCLazyTextEncode": "PC: Schedule Prompt",
+    "PCLazyTextEncodeAdvanced": "PC: Schedule prompt (Advanced)",
+    "PCLazyLoraLoader": "PC: Schedule LoRAs",
+    "PCLazyLoraLoaderAdvanced": "PC: Schedule LoRAs (Advanced)",
 }
