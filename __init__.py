2f5d62b46b849e0e39296b2924878f7ff2ae059a
asagi4 <130366179+asagi4@users.noreply.github.com>
2024-12-13 20:24:00 +0200

    Tag a non-broken release

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@
 @nickname: ComfyUI Prompt Control
 @description: Control LoRA and prompt scheduling, advanced text encoding, regional prompting, and much more, through your text prompt. Generates dynamic graphs that are literally identical to handcrafted noodle soup.
 """
+
 import os
 import sys
 import logging
