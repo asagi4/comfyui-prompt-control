c75d1a6651c17b50cff2f21eaed84111323599a7
asagi4 <130366179+asagi4@users.noreply.github.com>
2025-02-28 01:39:54 +0200

    Refactor tests a bit
    Also test that DEF handles whitespace as intended

--- prompt_control/test.py
+++ prompt_control/test.py
@@ -2,14 +2,18 @@ import unittest
 from .parser import parse_prompt_schedules as parse
 
 
+def prompt(until, text, *loras):
+    loras = {lora: {"weight": unet, "weight_clip": te} for lora, unet, te in loras}
+    return [until, {"prompt": text, "loras": loras}]
+
+
 class TestParser(unittest.TestCase):
-    def assertPrompt(self, p, step, text):
-        self.assertEqual(p[0], step)
-        self.assertEqual(p[1]["prompt"], text)
+    def assertPrompt(self, p, at, until, text, *loras):
+        self.assertEqual(p.at_step(at), prompt(until, text, *loras))
 
     def test_no_scheduling(self):
         p = parse("This is a (basic:0.6) (prompt) with [no scheduling] features")
-        expected = [1.0, {"prompt": "This is a (basic:0.6) (prompt) with [no scheduling] features", "loras": {}}]
+        expected = prompt(1.0, "This is a (basic:0.6) (prompt) with [no scheduling] features")
         self.assertEqual(p.at_step(0), expected)
         self.assertEqual(p.at_step(0.5), expected)
         self.assertEqual(p.at_step(1), expected)
@@ -18,26 +22,16 @@ class TestParser(unittest.TestCase):
         p = parse(
             "This is a (basic:0.6) (prompt) with (very [[simple]:(basic:0.6):0.5]:1.1) [features::0.8][ and this is ignored:1]"
         )
-        expected = [0.5, {"prompt": "This is a (basic:0.6) (prompt) with (very [simple]:1.1) features", "loras": {}}]
-        expected2 = [
-            0.8,
-            {"prompt": "This is a (basic:0.6) (prompt) with (very (basic:0.6):1.1) features", "loras": {}},
-        ]
-        expected3 = [1.0, {"prompt": "This is a (basic:0.6) (prompt) with (very (basic:0.6):1.1) ", "loras": {}}]
-        self.assertEqual(p.at_step(0), expected)
-        self.assertEqual(p.at_step(0.5), expected)
-        self.assertEqual(p.at_step(0.7), expected2)
-        self.assertEqual(p.at_step(1), expected3)
+        self.assertPrompt(p, 0, 0.5, "This is a (basic:0.6) (prompt) with (very [simple]:1.1) features")
+        self.assertPrompt(p, 0.5, 0.5, "This is a (basic:0.6) (prompt) with (very [simple]:1.1) features")
+        self.assertPrompt(p, 0.7, 0.8, "This is a (basic:0.6) (prompt) with (very (basic:0.6):1.1) features")
+        self.assertPrompt(p, 1.0, 1.0, "This is a (basic:0.6) (prompt) with (very (basic:0.6):1.1) ")
 
     def test_lora(self):
         p = parse("This is a (lora:0.6) (prompt) with [no scheduling] features <lora:foo:0.5> <lora:bar:0.5:1.0>")
-        expected = [
-            1.0,
-            {
-                "prompt": "This is a (lora:0.6) (prompt) with [no scheduling] features  ",
-                "loras": {"foo": {"weight": 0.5, "weight_clip": 0.5}, "bar": {"weight": 0.5, "weight_clip": 1.0}},
-            },
-        ]
+        expected = prompt(
+            1.0, "This is a (lora:0.6) (prompt) with [no scheduling] features  ", ("foo", 0.5, 0.5), ("bar", 0.5, 1.0)
+        )
         self.assertEqual(p.at_step(0), expected)
         self.assertEqual(p.at_step(0.5), expected)
         self.assertEqual(p.at_step(1), expected)
@@ -46,22 +40,15 @@ class TestParser(unittest.TestCase):
         p = parse(
             "This is a (lora:0.6) (prompt) with [scheduling] features [<lora:foo:0.5>:<lora:bar:0.5:0.2>:0.3] <lora:bar:0.5:1.0>"
         )
-        expected = [
+        self.assertPrompt(
+            p,
+            0.1,
             0.3,
-            {
-                "prompt": "This is a (lora:0.6) (prompt) with [scheduling] features  ",
-                "loras": {"foo": {"weight": 0.5, "weight_clip": 0.5}, "bar": {"weight": 0.5, "weight_clip": 1.0}},
-            },
-        ]
-        expected2 = [
-            1.0,
-            {
-                "prompt": "This is a (lora:0.6) (prompt) with [scheduling] features  ",
-                "loras": {"bar": {"weight": 1.0, "weight_clip": 1.2}},
-            },
-        ]
-        self.assertEqual(p.at_step(0.1), expected)
-        self.assertEqual(p.at_step(1), expected2)
+            "This is a (lora:0.6) (prompt) with [scheduling] features  ",
+            ("foo", 0.5, 0.5),
+            ("bar", 0.5, 1.0),
+        )
+        self.assertPrompt(p, 0.5, 1.0, "This is a (lora:0.6) (prompt) with [scheduling] features  ", ("bar", 1.0, 1.2))
 
     def test_seq(self):
         p = parse("This is a sequence of [SEQ:a:0.2::0.5:c:0.8][SEQ: and x:0.8]")
@@ -73,8 +60,8 @@ class TestParser(unittest.TestCase):
             1.0: "This is a sequence of ",
         }
         self.assertEqual(p.parsed_prompt, p2.parsed_prompt)
-        for k in prompts:
-            self.assertEqual(p.at_step(k), [k, {"prompt": prompts[k], "loras": {}}])
+        for k, v in prompts.items():
+            self.assertPrompt(p, k, k, v)
 
     def test_shortcuts_scheduling(self):
         p = parse("A schedule [a:0.1,0.7] b")
@@ -97,51 +84,48 @@ class TestParser(unittest.TestCase):
         for k in prompts:
             self.assertEqual(p.at_step(k), [prompts[k][0], {"prompt": prompts[k][1], "loras": {}}])
 
-        self.assertEqual(
-            p.at_step(0.6), [0.7, {"prompt": "This prompt is ", "loras": {"cool": {"weight": 1.0, "weight_clip": 1.0}}}]
-        )
-        self.assertEqual(
-            p.at_step(0.7), [0.7, {"prompt": "This prompt is ", "loras": {"cool": {"weight": 1.0, "weight_clip": 1.0}}}]
-        )
+        self.assertPrompt(p, 0.6, 0.7, "This prompt is ", ("cool", 1.0, 1.0))
+        self.assertPrompt(p, 0.7, 0.7, "This prompt is ", ("cool", 1.0, 1.0))
         p2 = p.with_filters(filters="hr, xyz")
 
         self.assertEqual(p2.at_step(0), p2.at_step(1))
 
     def test_def(self):
-        p = parse("DEF(X=0.5) [a:b:X] DEF(test=[c:X]) test test")
+        p = parse("DEF(X=0.5) [a:b:X] DEF(test = [c:X]) test test")
         prompts = {
             0.2: (0.5, "a   "),
             0.6: (1.0, "b  c c"),
         }
-        for k in prompts:
-            self.assertEqual(p.at_step(k), [prompts[k][0], {"prompt": prompts[k][1], "loras": {}}])
+        for k, v in prompts.items():
+            self.assertPrompt(p, k, v[0], v[1])
 
     def test_misc(self):
         p = parse("[[a:c:0.5]:0.7]")
         p2 = parse("[:[a:c:0.5]:0.7]")
         self.assertEqual(p.parsed_prompt, p2.parsed_prompt)
+
         p = parse("test [[a:[b<lora:test:0.5>:0.6]:0.5]:HR]")
         p2 = parse("test [:[a:[:b<lora:test:0.5>:0.6]:0.5]:HR]")
         self.assertEqual(p.parsed_prompt, p2.parsed_prompt)
+
         pf = p.with_filters(filters="hr")
         self.assertEqual(pf.parsed_prompt, p2.with_filters(filters="hr").parsed_prompt)
-        self.assertEqual(pf.at_step(0), [0.5, {"prompt": "test a", "loras": {}}])
-        self.assertEqual(pf.at_step(0.55), [0.6, {"prompt": "test ", "loras": {}}])
-        self.assertEqual(
-            pf.at_step(0.8), [1.0, {"prompt": "test b", "loras": {"test": {"weight": 0.5, "weight_clip": 0.5}}}]
-        )
+        self.assertPrompt(pf, 0, 0.5, "test a")
+        self.assertPrompt(pf, 0.55, 0.6, "test ")
+        self.assertPrompt(pf, 0.8, 1.0, "test b", ("test", 0.5, 0.5))
+
         p = parse("[:[<lora:test:1>:c:0.5]:0.3]")
-        self.assertEqual(p.at_step(0), [0.3, {"prompt": "", "loras": {}}])
-        self.assertEqual(p.at_step(0.4), [0.5, {"prompt": "", "loras": {"test": {"weight": 1.0, "weight_clip": 1.0}}}])
-        self.assertEqual(p.at_step(1.0), [1.0, {"prompt": "c", "loras": {}}])
+        self.assertPrompt(p, 0, 0.3, "")
+        self.assertPrompt(p, 0.4, 0.5, "", ("test", 1.0, 1.0))
+        self.assertPrompt(p, 1.0, 1.0, "c")
 
         p = parse("an [<emb:foo>:<emb:bar>:0.5]")
         prompts = {
             0.2: (0.5, "an embedding:foo"),
             0.8: (1.0, "an embedding:bar"),
         }
-        for k in prompts:
-            self.assertEqual(p.at_step(k), [prompts[k][0], {"prompt": prompts[k][1], "loras": {}}])
+        for k, v in prompts.items():
+            self.assertPrompt(p, k, v[0], v[1])
 
     def test_alternating(self):
         p = parse("[cat|dog|tiger]")
@@ -152,12 +136,12 @@ class TestParser(unittest.TestCase):
         self.assertEqual(p.parsed_prompt, p2.parsed_prompt)
         for i, x in enumerate(["cat", "wolf", "tiger", "cat", "dog", "tiger", "cat", "wolf", "tiger", "cat"]):
             step = round((i * 0.1) + 0.1, 2)
-            self.assertPrompt(p3.at_step(step), step, x)
+            self.assertPrompt(p3, step, step, x)
 
-        for i, x in enumerate(["cat", "dog", "cat", "wolf", "cat"]):
+        for i, x in enumerate([["cat"], ["dog"], ["cat"], ["wolf", ("canine", 1.0, 1.0)], ["cat"]]):
             step = round((i * 0.2) + 0.2, 2)
-            self.assertPrompt(p4.at_step(step), step, x)
-        self.assertEqual(p4.at_step(0.7)[1]["loras"], {"canine": {"weight": 1.0, "weight_clip": 1.0}})
+            self.assertPrompt(p4, step, step, *x)
+        self.assertPrompt(p4, 0.7, 0.8, "wolf", ("canine", 1.0, 1.0))
 
 
 if __name__ == "__main__":
