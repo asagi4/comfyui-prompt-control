192e6d30d46b2991c7b4f1d662a8cf673a372866
asagi4 <130366179+asagi4@users.noreply.github.com>
2025-06-07 20:30:56 +0300

    refactor adv_encode

--- prompt_control/adv_encode.py
+++ prompt_control/adv_encode.py
@@ -1,6 +1,9 @@
 import torch
 import numpy as np
-from math import copysign, lcm
+from math import copysign
+import logging
+
+log = logging.getLogger("comfyui-prompt-control")
 
 
 def _norm_mag(w, n):
@@ -164,6 +167,271 @@ def perp_weight(weights, unweighted_embs, empty_embs):
     return result, unweighted_pooled
 
 
+def style_comfy(encoder, tokens, **kwargs):
+    tokens = encoder.without_word_ids(tokens)
+    return encoder.encode_fn(tokens)
+
+
+def style_a1111(encoder, tokens, **kwargs):
+    base_emb, pooled = encoder.base_emb(tokens)
+    weighted_emb = base_emb * weights_like(encoder.weights(tokens), base_emb)
+    weighted_emb = (base_emb.mean() / weighted_emb.mean()) * weighted_emb  # renormalize
+    return weighted_emb, pooled
+
+
+def style_compel(encoder, tokens, **kwargs):
+    pos_tokens = encoder.weighted_with(tokens, lambda w: w if w > 1.0 else 1.0)
+    weighted_emb, pooled = encoder.encode_fn(pos_tokens)
+    weighted_emb, _, pooled = encoder.down_weight(
+        pos_tokens, encoder.weights(tokens), encoder.word_ids(tokens), weighted_emb, pooled
+    )
+    return weighted_emb, pooled
+
+
+def style_comfypp(encoder, tokens, **kwargs):
+    unweighted_tokens = encoder.unweighted(tokens)
+    base_emb, pooled_base = encoder.base_emb(tokens)
+    weighted_emb, tokens_down, _ = encoder.down_weight(
+        unweighted_tokens, encoder.weights(tokens), encoder.word_ids(tokens), base_emb, pooled_base
+    )
+    weights = encoder.weights(encoder.weighted_with(tokens, lambda w: w if w > 1.0 else 1.0))
+    embs, pooled = encoder.from_masked(
+        unweighted_tokens,
+        weights,
+        encoder.word_ids(tokens),
+        base_emb,
+        pooled_base,
+    )
+    weighted_emb += embs
+
+    return weighted_emb, pooled
+
+
+def style_downweight(encoder, tokens, **kwargs):
+    weights = scale_to_norm(encoder.weights(tokens), encoder.word_ids(tokens), encoder.w_max)
+    base_emb, pooled_base = encoder.base_emb(tokens)
+    weighted_emb, _, pooled = encoder.down_weight(
+        encoder.unweighted(tokens), weights, encoder.word_ids(tokens), base_emb, pooled_base
+    )
+
+    return weighted_emb, pooled
+
+
+def style_perp(encoder, tokens, **kwargs):
+    zero_emb, _ = encoder.encode_fn(encoder.tokenizer.tokenize_with_weights(""))
+    base_emb, pooled = encoder.base_emb(tokens)
+    return perp_weight(encoder.weights(tokens), (base_emb, pooled), (zero_emb, None))
+
+
+def apply_negpip(encoder, emb, pooled, **kwargs):
+    original_tokens = kwargs["original_tokens"]
+    emb_negpip = torch.empty_like(emb).repeat(1, 2, 1)
+    emb_negpip[:, 0::2, :] = emb
+    emb_negpip[:, 1::2, :] = emb * weights_like(encoder.signs(original_tokens), emb)
+    return emb_negpip, pooled
+
+
+def norm_length(encoder, tokens, **kwargs):
+    word_ids = encoder.word_ids(tokens)
+    sums = dict(zip(*np.unique(word_ids, return_counts=True)))
+    sums[0] = 1
+    tokens = [[(t, _norm_mag(w, sums[id]) if id != 0 else 1.0, id) for (t, w, id) in x] for x in tokens]
+    return tokens
+
+
+def norm_mean(encoder, tokens, **kwargs):
+    weights = encoder.weights(tokens)
+    word_ids = encoder.word_ids(tokens)
+    delta = 1 - np.mean([w for x, y in zip(weights, word_ids) for w, id in zip(x, y) if id != 0])
+    tokens = [[(t, w if id == 0 else w + delta, id) for (t, w, id) in x] for x in tokens]
+    return tokens
+
+
+def norm_none(encoder, tokens, **kwargs):
+    return tokens
+
+
+class AdvancedEncoder:
+    STYLES = {
+        "A1111": style_a1111,
+        "comfy": style_comfy,
+        "comfy++": style_comfypp,
+        "compel": style_compel,
+        "down_weight": style_downweight,
+        "perp": style_perp,
+    }
+    NORMALIZATION_OPS = {
+        "none": norm_none,
+        "length": norm_length,
+        "mean": norm_mean,
+    }
+
+    @classmethod
+    def add_encoder(cls, name, fn):
+        cls.STYLES[name] = fn
+
+    def add_normalization_op(cls, name, fn):
+        cls.NORMALIZATION_OPS[name] = fn
+
+    @classmethod
+    def weighted_with(cls, tokens, fn=id, word_ids=True):
+        w = ([(t, fn(w), id) for t, w, id in x] for x in tokens)
+        if not word_ids:
+            w = cls.without_word_ids(w)
+        return list(w)
+
+    @classmethod
+    def unweighted(cls, tokens, word_ids=False):
+        return cls.weighted_with(tokens, fn=lambda w: 1.0, word_ids=word_ids)
+
+    @classmethod
+    def tokens_only(cls, tokens):
+        return list([t[0] for t in x] for x in tokens)
+
+    @classmethod
+    def weights(cls, tokens):
+        return list([t[1] for t in x] for x in tokens)
+
+    @classmethod
+    def word_ids(cls, tokens):
+        return list([t[2] for t in x] for x in tokens)
+
+    @classmethod
+    def signs(cls, tokens):
+        return list([copysign(1, t[1]) for t in x] for x in tokens)
+
+    @classmethod
+    def without_word_ids(cls, tokens):
+        return list([(t, w) for t, w, _ in x] for x in tokens)
+
+    def __init__(self, encode_fn, style, normalization, tokenizer, m_token="+", w_max=1.0, **extra_args):
+        self.encode_fn = encode_fn
+        self.preprocessors = []
+        self.postprocessors = []
+        self.tokenizer = tokenizer
+        self.extra_args = extra_args
+        self.m_token = tokenizer.tokenize_with_weights(m_token)[0][tokenizer.tokens_start]
+        self.max_length = tokenizer.max_length if tokenizer.pad_to_max_length else None
+        self.w_max = w_max
+
+        norms = normalization.split("+")
+        assert style in self.STYLES, f"Invalid weight interpretation: {style}"
+        self.weight_fn = self.STYLES[style]
+        for n in norms:
+            n = n.strip()
+            assert n in self.NORMALIZATION_OPS, f"Invalid normalization: {normalization}"
+            self.preprocessors.append(self.NORMALIZATION_OPS[n])
+
+        negpip = extra_args.get("has_negpip")
+        if negpip:
+
+            def _encode(t):
+                emb, pooled = encode_fn(t)
+                return emb[:, 0::2, :], pooled
+
+            self.encode_fn = _encode
+            self.preprocessors.insert(lambda encoder, tokens, **kwargs: encoder.weighted_with(tokens, abs))
+            self.postprocessors.insert(0, apply_negpip)
+
+    def base_emb(self, tokens):
+        unweighted = self.unweighted(tokens)
+        return self.encode_fn(unweighted)
+
+    def down_weight(self, tokens, weights, word_ids, base_emb, pooled_base):
+        w, w_inv = np.unique(weights, return_inverse=True)
+
+        if np.sum(w < 1) == 0:
+            return (
+                base_emb,
+                tokens,
+                (
+                    base_emb[0, self.max_length - 1 : self.max_length, :]
+                    if (pooled_base is not None and self.max_length)
+                    else None
+                ),
+            )
+
+        m_token = (self.m_token, 1.0)
+
+        masked_tokens = []
+
+        masked_current = tokens
+        for i in range(len(w)):
+            if w[i] >= 1:
+                continue
+            masked_current = mask_inds(masked_current, np.where(w_inv == i)[0], m_token)
+            masked_tokens.extend(masked_current)
+
+        embs, pooled = self.encode_fn(tokens)
+        embs = torch.cat([base_emb, embs])
+        w = w[w <= 1.0]
+        w_mix = np.diff([0] + w.tolist())
+        w_mix = torch.tensor(w_mix, dtype=embs.dtype, device=embs.device).reshape((-1, 1, 1))
+
+        weighted_emb = (w_mix * embs).sum(axis=0, keepdim=True)
+        if pooled is not None and self.max_length:
+            pooled = weighted_emb[0, self.max_length - 1 : self.max_length, :]
+        return weighted_emb, masked_current, pooled
+
+    def from_masked(self, tokens, weights, word_ids, base_emb, pooled_base):
+        wids, inds = np.unique(np.array(word_ids).reshape(-1), return_index=True)
+        weight_dict = dict((id, w) for id, w in zip(wids, np.array(weights).reshape(-1)[inds]) if w != 1.0)
+
+        if len(weight_dict) == 0:
+            return torch.zeros_like(base_emb), torch.zeros_like(pooled_base) if pooled_base is not None else None
+
+        weight_tensor = weights_like(weights, base_emb)
+
+        m_token = (self.m_token, 1.0)
+
+        ws = []
+        masked_tokens = []
+        masks = []
+
+        # create prompts
+        for id, w in weight_dict.items():
+            masked, m = mask_word_id(tokens, word_ids, id, m_token)
+            masked_tokens.extend(masked)
+            masks.append(weights_like(m, base_emb))
+
+            ws.append(w)
+
+        embs, pooled = self.encode_fn(tokens)
+        masks = torch.cat(masks)
+
+        embs = base_emb.expand(embs.shape) - embs
+        if pooled is not None and self.max_length:
+            pooled = embs[0, self.max_length - 1 : self.max_length, :]
+            pooled_start = pooled_base.expand(len(ws), -1)
+            ws = torch.tensor(ws).reshape(-1, 1).expand(pooled_start.shape)
+            pooled = (pooled - pooled_start) * (ws - 1)
+            pooled = pooled.mean(axis=0, keepdim=True)
+            pooled = pooled_base + pooled
+
+        if embs.shape[0] != masks.shape[0]:
+            embs = embs.repeat(masks.shape[0], 1, 1)
+        embs *= masks
+        embs = embs.sum(axis=0, keepdim=True)
+
+        return ((weight_tensor - 1) * embs), pooled
+
+    def __call__(self, tokens, apply_to_pooled=False, return_pooled=False):
+        normalized_tokens = tokens
+        for op in self.preprocessors:
+            normalized_tokens = op(self, normalized_tokens)
+
+        emb, pooled = self.weight_fn(self, normalized_tokens, original_tokens=tokens)
+
+        for fn in self.postprocessors:
+            emb, pooled = fn(self, emb, pooled, tokens=tokens, original_tokens=tokens)
+
+        if return_pooled:
+            if not apply_to_pooled:
+                _, pooled = self.base_emb(tokens)
+            return emb, pooled
+        return emb, None
+
+
 def advanced_encode_from_tokens(
     tokenized,
     token_normalization,
@@ -174,8 +442,16 @@ def advanced_encode_from_tokens(
     return_pooled=False,
     apply_to_pooled=False,
     tokenizer=None,
-    **extra_args
+    **extra_args,
 ):
+    if "new+" in weight_interpretation:
+        weight_interpretation = weight_interpretation.replace("new+", "")
+        enc = AdvancedEncoder(
+            encode_func, weight_interpretation, token_normalization, tokenizer, m_token, w_max, **extra_args
+        )
+        log.info("Using new implementation for %s", weight_interpretation)
+        return enc(tokenized, return_pooled=return_pooled, apply_to_pooled=apply_to_pooled)
+
     negpip = extra_args.get("has_negpip")
     if negpip:
         weights_sign = [[copysign(1, w) for _, w, _ in x] for x in tokenized]
@@ -189,10 +465,10 @@ def advanced_encode_from_tokens(
         encode_func = _encode
 
     assert tokenizer, "Must pass tokenizer"
+    m_token = tokenizer.tokenize_with_weights(m_token)[0][tokenizer.tokens_start]
     max_length = None
     if tokenizer.pad_to_max_length:
         max_length = tokenizer.max_length
-    m_token = tokenizer.tokenize_with_weights(m_token)[0][tokenizer.tokens_start]
 
     tokens = [[t for t, _, _ in x] for x in tokenized]
     weights = [[w for _, w, _ in x] for x in tokenized]
--- prompt_control/prompts.py
+++ prompt_control/prompts.py
@@ -65,9 +65,8 @@ def get_style(text, default_style="comfy", default_normalization="none"):
     style, normalization = styles[0]
     style = style.strip()
     normalization = normalization.strip()
-    if style not in AVAILABLE_STYLES:
+    if style.replace("new+", "") not in AVAILABLE_STYLES:
         log.warning("Unrecognized prompt style: %s. Using %s", style, default_style)
-        style = default_style
 
     for part in normalization.split("+"):
         if part not in AVAILABLE_NORMALIZATIONS:
