c74e628ed9dc93a3d84fb4e080051cbe060ae6f4
asagi4 <130366179+asagi4@users.noreply.github.com>
2025-12-30 12:40:49 +0200

    Make github tests work again

--- __init__.py
+++ __init__.py
@@ -5,19 +5,11 @@
 @description: Control LoRA and prompt scheduling, advanced text encoding, regional prompting, and much more, through your text prompt. Generates dynamic graphs that are literally identical to handcrafted noodle soup.
 """
 
-import importlib
 import logging
 import os
 import sys
-from dataclasses import dataclass
-
-from comfy_api.latest import ComfyExtension
 
 log = logging.getLogger("comfyui-prompt-control")
-if not log.handlers and "PYTEST_CURRENT_TEST" not in os.environ:
-    h = logging.StreamHandler(sys.stdout)
-    h.setFormatter(logging.Formatter("[PromptControl] %(levelname)s: %(message)s"))
-    log.addHandler(h)
 
 if os.environ.get("PROMPTCONTROL_DEBUG"):
     log.setLevel(logging.DEBUG)
@@ -30,18 +22,24 @@ v1_modules = []
 v3_modules = []
 # Importing things here breaks pytest for whatever reason...
 if "PYTEST_CURRENT_TEST" not in os.environ:
+    import importlib
+
+    from comfy_api.latest import ComfyExtension
+
+    if not log.handlers:
+        h = logging.StreamHandler(sys.stdout)
+        h.setFormatter(logging.Formatter("[PromptControl] %(levelname)s: %(message)s"))
+        log.addHandler(h)
     for node in ["base", "hooks", "tools", "lazy"]:
         mod = importlib.import_module(f".prompt_control.nodes_{node}", package=__name__)
         v3_modules.append(mod)
 
+    class PromptControlExtension(ComfyExtension):
+        async def get_node_list(self):
+            r = []
+            for m in v3_modules:
+                r.extend(m.NODES)
+            return r
 
-class PromptControlExtension(ComfyExtension):
-    async def get_node_list(self):
-        r = []
-        for m in v3_modules:
-            r.extend(m.NODES)
-        return r
-
-
-async def comfy_entrypoint():
-    return PromptControlExtension()
+    async def comfy_entrypoint():
+        return PromptControlExtension()
