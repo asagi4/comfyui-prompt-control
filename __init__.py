3fcd708dc637b220e54f0b1995cd051ec0ebc286
asagi4 <130366179+asagi4@users.noreply.github.com>
2025-12-30 10:00:23 +0200

    Working importing for v3 nodes

--- __init__.py
+++ __init__.py
@@ -26,45 +26,18 @@ else:
 
 WEB_DIRECTORY = "web"
 
-nodes = ["lazy", "tools"]
-v3_nodes = ["base", "hooks"]
-if "PYTEST_CURRENT_TEST" in os.environ:
-    nodes = []
-
 v1_modules = []
-for node in nodes:
-    mod = importlib.import_module(f".prompt_control.nodes_{node}", package=__name__)
-    v1_modules.append(mod)
-
 v3_modules = []
-for node in v3_nodes:
-    mod = importlib.import_module(f".prompt_control.nodes_{node}", package=__name__)
-    v1_modules.append(mod)
-
-
-@dataclass
-class PCSchemaStub:
-    node_id: str
-    display_name: str
-
-
-def v3_stub(module):
-    def inject(cls, node_id, display_name):
-        schema = PCSchemaStub(node_id, display_name)
-        if not hasattr(cls, "GET_SCHEMA"):
-            setattr(cls, "GET_SCHEMA", lambda: schema)
-        return cls
-
-    return [
-        inject(cls, name, module.NODE_DISPLAY_NAME_MAPPINGS[name]) for name, cls in module.NODE_CLASS_MAPPINGS.items()
-    ]
+# Importing things here breaks pytest for whatever reason...
+if "PYTEST_CURRENT_TEST" not in os.environ:
+    for node in ["base", "hooks", "tools", "lazy"]:
+        mod = importlib.import_module(f".prompt_control.nodes_{node}", package=__name__)
+        v3_modules.append(mod)
 
 
 class PromptControlExtension(ComfyExtension):
     async def get_node_list(self):
         r = []
-        for m in v1_modules:
-            r.extend(v3_stub(m))
         for m in v3_modules:
             r.extend(m.NODES)
         return r
