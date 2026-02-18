de79f3c6afe8ec147be98ef5d5733d24a7a60f27
asagi4 <130366179+asagi4@users.noreply.github.com>
2026-02-18 20:17:46 +0200

    Properly default to new parser

--- prompt_control/parser.py
+++ prompt_control/parser.py
@@ -3,7 +3,7 @@ import os
 
 log = logging.getLogger("comfyui-prompt-control")
 
-if os.environ.get("PC_USE_OLD_PARSER", "0") != "0":
+if os.environ.get("PC_USE_OLD_PARSER", "0") != "1":
     log.info("Using new parser implementation. Set PC_USE_OLD_PARSER=1 to use old parser instead")
     from .parser_parsy import parse_prompt_schedules  # noqa
 else:
